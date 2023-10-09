898d991a95ec8d7f1fbf2e0da47d2c6697fa8a17
ocsoares <casoares016@gmail.com>
2023-10-09 17:57:29 -0300

    chore: delete useless error files

--- src/exceptions/user-exceptions/error-deleting-user.exception.ts
+++
@@ -1,7 +0,0 @@
-import { InternalServerErrorException } from '@nestjs/common';
-
-export class ErrorDeletingUserException extends InternalServerErrorException {
-    constructor() {
-        super('Error deleting user !');
-    }
-}
--- src/exceptions/user-exceptions/error-updating-user.exception.ts
+++
@@ -1,7 +0,0 @@
-import { InternalServerErrorException } from '@nestjs/common';
-
-export class ErrorUpdatingUserException extends InternalServerErrorException {
-    constructor() {
-        super('Error updating user !');
-    }
-}

fbd4512de0bedec20fd3b30080280b199e37718d
ocsoares <casoares016@gmail.com>
2023-10-11 13:22:30 -0300

    feat: change email exists http error to Conflict

--- src/exceptions/user-exceptions/user-already-exists-by-email.exception.ts
+++ src/exceptions/user-exceptions/user-already-exists-by-email.exception.ts
@@ -1,6 +1,6 @@
-import { BadRequestException } from '@nestjs/common';
+import { ConflictException } from '@nestjs/common';
 
-export class UserAlreadyExistsByEmailException extends BadRequestException {
+export class UserAlreadyExistsByEmailException extends ConflictException {
     constructor() {
         super('Already exists a user registered with this email !');
     }

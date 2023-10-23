bcecc07646131a956c6719c27fdc6cc9e96faa43
ocsoares <casoares016@gmail.com>
2023-10-23 19:28:42 -0300

    feat: add rate limiter middleware

--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,16 @@ import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { PORT } from './config/app';
+import { rateLimiterMiddleware } from './modules/auth/middlewares/rate-limiter.middleware';
+import { mw } from 'request-ip';
 
 async function bootstrap() {
     const app = await NestFactory.create(AppModule);
+
+    app.use(mw());
+
+    // I used this because NestJS Throttler Module doesn't work !!!
+    app.use(rateLimiterMiddleware);
 
     app.setGlobalPrefix('api');
 

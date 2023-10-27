3f24ecb914d91429de67552331295634ea6ed37d
ocsoares <casoares016@gmail.com>
2023-10-26 21:51:06 -0300

    chore: set env. variable to JWT module expiresIn

--- src/modules/user/user.module.ts
+++ src/modules/user/user.module.ts
@@ -14,7 +14,7 @@ import { GenerateUserGoogleTokenService } from './use-cases/generate-user-google
         JwtModule.registerAsync({
             useFactory: async () => ({
                 secret: process.env.JWT_SECRET,
-                signOptions: { expiresIn: '60s' },
+                signOptions: { expiresIn: process.env.JWT_EXPIRATION },
             }),
         }),
     ],
